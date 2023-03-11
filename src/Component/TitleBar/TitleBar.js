ae0377f2f3fcadf795b484476b1c3f80192e4a55
swayam <goura0775@gmail.com>
2023-03-11 11:38:02 +0530

    helo

--- src/Component/TitleBar/TitleBar.js
+++ src/Component/TitleBar/TitleBar.js
@@ -5,7 +5,10 @@ export default function TitleBar() {
   return (
     <div className={style.main}>
        <img className={style.img} src="https://thecanadiantechie.files.wordpress.com/2015/09/google_2015.gif" alt="img" />
-     
+     <div>
+      
+     </div>
     </div>
+    
   )
 }
