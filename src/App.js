4ddcd2ead2ad08da34eed4854e2a5132f2859455
sneha6gour <goursneha704@gmail.com>
2023-04-20 17:28:18 +0530

    done1

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </header>
+        <div className="App12">
+        <h1>Benifits of using react...</h1>
+      <ol>
+       <li>Component-based archifecture</li>
+       <li>Virtule DOM for efficient updates</li>
+       <li>Rich ecosystem and community</li>
+       <li>Cross-platform development</li>
+       <li>Storng community support</li>
+      </ol> 
+      <button>Get Started</button>
+      </div>
+      
+      
     </div>
   );
 }
