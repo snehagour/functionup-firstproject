3a3dbe5f8e41787df9ab7bf97bba806dd7a17401
sneha6gour <goursneha704@gmail.com>
2023-05-03 10:00:23 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState } from 'react';
+import { NewObjcets } from './arry'
 function App() {
+let api = "https://reqres.in/api/users/"
+
+let [array,setArr] = useState([])
+async function handlegetUser(){
+  
+    
+    const response = await fetch(api).then((res)=>res);
+    const arr = await response.json();
+    setArr(arr.data)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+  {array.map((el,index) => {
+    return <NewObjcets
+    indexs={el}
+    key={array.id}
+    />
+  })}
+  <button onClick={handlegetUser}>get user</button>
+     
     </div>
   );
 }
---
+++ src/arry.js
@@ -0,0 +1,14 @@
+ import "./arry.css"
+ export function NewObjcets({indexs}){
+
+    return(
+
+        <div className="elements">
+         <h2>{indexs.id}</h2>
+         <h3>{indexs.email}</h3>
+         <h3>{indexs.first_name}{indexs.last_name}</h3>
+         <img src={indexs.avatar} alt="avatar"/>
+
+        </div>
+    )
+    }
