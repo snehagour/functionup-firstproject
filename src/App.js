62afb8c704558745e59eb6483c9bba7ca858293a
sneha6gour <goursneha704@gmail.com>
2023-05-04 09:59:15 +0530

    mobilenumber

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ const [mobilenumber,setMobilenumber] =useState()
+  
+ function handle(){
+  if(mobilenumber < 999967896 || mobilenumber >= 56567890342){
+    alert ("Invalid mobile number")
+    console.error("invalid number")
+    return;
+  }
+  fetch( "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP",{
+    method:"POST",
+    headers:{
+      "content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      mobile: mobilenumber
+    })
+  })
+
+     .then((res)=> res.json())
+     .then((data)=> {
+      setMobilenumber(" ");
+      alert("succesfully")
+      console.log(data);
+     })
+     .catch(error=>{
+      alert("plase retry, somthing went wrong")
+      console.error(error);
+     });
+      }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form
+      onSubmit={e=> e.preventDefault()}
+      noValidate
+      autoComplete='off'
+      
+      >
+        <input type="number" placeholder='mobile number' 
+        validation={{
+          required:{
+            value:"true",
+            message:"required",
+          },
+          minLength:{
+            value:10,
+            message: "min 10 character",
+          },
+          maxLength:{
+            value:10,
+            message:"min 10 character",
+          }
+        }}
+        onChange={e=>setMobilenumber(e.target.value)}
+        value={mobilenumber}
+        ></input>
+      </form>
+      <button type="sumit" onClick={handle}>Get otp</button>
     </div>
   );
 }
