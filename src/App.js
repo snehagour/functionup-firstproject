b43591a3b6a1820d72d6da518b6d6eb7516c1152
sneha6gour <goursneha704@gmail.com>
2023-05-05 09:38:49 +0530

    img axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import {useEffect, useState} from "react"
+import axios from "axios";
 import './App.css';
 
 function App() {
-  const[image,setImage]=useState()
+  const[image,setImage]=useState( )
+
+
+  
  useEffect(()=>{
-  async function getDog(){
-    try{
-      const response = await fetch ("https://dog.ceo/api/breeds/image/random")
-      const data = await response.json()
-      setImage(data.message)
-    }
-    catch(error){
-    console.log("i am from catch", error)
-    }
-  }
-  setImage(getDog)
+  
+  axios.get("https://dog.ceo/api/breeds/image/random")
+  .then((response)=>setImage(response.data.message))
+  
  },[])
+  
+ 
+
   return (
     <div className="App">
-     <img src={image}   alt="image" height={"300px"} />
+     <img src={image}  alt="image" height={"300px"} />
     </div>
   );
 }
