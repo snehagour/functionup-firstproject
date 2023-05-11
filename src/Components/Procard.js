2e205e2bcc403515ead225c62bb25ca4389182fd
sneha6gour <goursneha704@gmail.com>
2023-05-11 11:27:46 +0530

    css2

---
+++ src/Components/Procard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Procard.css" 
+
+function Procard() {
+  return (
+    <>
+    <div className='card-container'>
+      
+      <div className='container'><img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"  /><p> Rani duvedi  <br/>ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg"  /><p>Rahul duvedi  <br/>japan SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container' ><img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"  /><p> Rani duvedi  <br/>ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      
+      <div className='container'><img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg" /><p> Rahul duvedi <br/>japan SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://live.staticflickr.com/3666/10335709503_8437c0f514_z.jpg"  /><p>Rohit sheety  <br/>Udaypur SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg"/><p> Shyam sunder <br/> ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY</p></div>
+      <div className='container'><img src="https://i.pinimg.com/originals/25/3a/bf/253abf4f1f4bc16b6dc04571f8d21624.png" /><p>Aditya duvedi<br/>RAMji SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"/><p> ajay duvedi <br/>JINT SCIENCE AND TECHNOLOGY UNIVERSITY</p></div>
+      <div className='container'><img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"/><p>Rani duvedi <br/>ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY</p></div>
+      <div className='container'><img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"  /><p> Rani duvedi  <br/>ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg"  /><p>Rahul duvedi  <br/>japan SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container' ><img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"  /><p> Rani duvedi  <br/>ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      
+      <div className='container'><img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?cs=srgb&dl=pexels-italo-melo-2379005.jpg&fm=jpg" /><p> Rahul duvedi <br/>japan SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://live.staticflickr.com/3666/10335709503_8437c0f514_z.jpg"  /><p>Rohit sheety  <br/>Udaypur SCIENCE AND TECHNOLOGY UNIVERSITY </p></div>
+      <div className='container'><img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-2379004.jpg&fm=jpg"/><p> Shyam sunder <br/> ASSAM SCIENCE AND TECHNOLOGY UNIVERSITY</p></div>
+
+       
+      
+      </div>
+    </>
+  )
+}
+export default Procard;
+
