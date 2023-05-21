6d15af7e5ce1c0bb27ad5c4215a008c713d1c5f1
snehagour <119596424+snehagour@users.noreply.github.com>
2023-05-21 22:15:40 +0530

    Update QuizeData.js

--- src/DATA/QuizeData.js
+++ src/DATA/QuizeData.js
@@ -4,40 +4,40 @@
     {
         question:"  Which city is the host of the QUAD Summit 2023?",
         options:[" New Delhi","Sydney"  ,"Tokyo" ,"New York",],
-        answer: "2",
+        answer: 2,
         
     },
     {
         question:"  Which institution has launched the liquid nano Di-Ammonia Phosphate (DAP) in India?",
         options:["NITI Aayog","NABARD","IFFCO","FCI"],
-        answer: "3" ,  
+        answer: 3,  
 
 
     },
     {
         question:"  Which city is the host of the United Nations Permanent Forum on Indigenous Issues?",
         options:["New Delhi"," New York","Sydney","  London"],
-        answer: "2" ,  
+        answer: 2 ,  
 
 
     },
     {
         question:"  Which city is the host of the QUAD Summit 2023?",
         options:[" New Delhi","Sydney"  ,"Tokyo" ,"New York",],
-        answer: "2",
+        answer: 2,
         
     },
     {
         question:"  Which institution has launched the liquid nano Di-Ammonia Phosphate (DAP) in India?",
         options:["NITI Aayog","NABARD","IFFCO","FCI"],
-        answer: "3"   ,
+        answer: 3   ,
 
 
     },
     {
         question:"  Which city is the host of the United Nations Permanent Forum on Indigenous Issues?",
         options:["New Delhi"," New York","Sydney","  London"],
-        answer: "2"  , 
+        answer: 2 , 
 
 
     },
