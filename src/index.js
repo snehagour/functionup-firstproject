96663f3f42c91d52e929c19d8425c962e9848ba5
snehagour <119596424+snehagour@users.noreply.github.com>
2023-05-27 23:18:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { Provider } from "react-redux"
 
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App  /> 
+    <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
