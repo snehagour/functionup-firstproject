3cd83da5a58d6aeb0f320341c038e1e06931eb15
sneha6gour <goursneha704@gmail.com>
2023-06-30 10:14:36 +0530

    calculater

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import {useState} from 'react'
+export default function App() {
+  const[data,setdata]=useState()
+  const[calculate,setcalculate]=useState()
+  const[result,setresult]=useState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator'>
+    <br/><br/><br/>
+    <input style={{width:"200px",height:"30px"}} type='text'
+     placeholder='enter first number'
+     onChange={((e)=>{setdata(e.target.value)})} />
+      <br/><br/>
+      <input  style={{width:"200px",height:"30px"}} placeholder='enter second number'
+      type='text' onChange={((e)=>{ setcalculate(e.target.value)})} /> <br/><br/>
+      <br/><button onClick={(()=>{setresult(Number(data)+Number(calculate) )})}>addition</button>
+      <button onClick={(()=>{setresult(data-calculate)})}>substraction</button>
+      <button onClick={(()=>{setresult(data*calculate)})}>multiply</button>
+      <button onClick={(()=>{setresult(data/calculate)})}>divide</button>
+      <br/> <h1> {result}</h1></div>
+
     </div>
   );
 }
-
-export default App;
